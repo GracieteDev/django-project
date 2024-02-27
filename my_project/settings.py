8a41c1406f97ac4ef9969e3765b1257387222a09
Graciete Gil <graciete.gil@gmail.com>
2024-02-27 17:49:16 +0000

    Install webserver gunicorn for Heroku, add it to requirements.txt.  Create Procfile. Update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&=af%^tkxv38be8s(grw5+%ry3#+li%ba6pg)!@f=x@bbqbs@!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition

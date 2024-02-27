a5e6bb1702eb427ba6baf14252555cdc5878e369
Graciete Gil <graciete.gil@gmail.com>
2024-02-27 16:55:56 +0000

    adjust view with request method POST

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
-    return HttpResponse("Hello, world!")
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
